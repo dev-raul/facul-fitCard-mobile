7f112f69462d5c2553dd9ed1a5b874f97614ce42
Raul Silva Sousa <rraulsilva.dev@gmail.com>
2020-04-29 22:46:48 -0300

    Adding Redux

--- src/App.js
+++ src/App.js
@@ -9,17 +9,22 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import './configs/reactotron';
 import Routes from './routes';
 import {AuthProvider} from '~/context/auth';
+import {Provider} from 'react-redux';
+import {store} from './store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <AuthProvider>
-        <StatusBar backgroundColor="#e02041" barStyle="light-content" />
-        <Routes loggedIn={false} />
-      </AuthProvider>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AuthProvider>
+          <StatusBar backgroundColor="#e02041" barStyle="light-content" />
+          <Routes loggedIn={false} />
+        </AuthProvider>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
